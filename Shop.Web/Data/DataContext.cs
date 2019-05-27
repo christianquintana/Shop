d11cae61d7de306b21ba053b8c08a70f964d993d
Christian Quintana <ceqn_20@hotmail.com>
2019-05-27 17:48:51 -0500

    Implementing login and logout in Web

--- Shop.Web/Data/DataContext.cs
+++ Shop.Web/Data/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿namespace Shop.Web.Data
 {
+    using System.Linq;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
     using Entities;
@@ -7,11 +8,31 @@
     public class DataContext : IdentityDbContext<User> //DbContext
     {
         public DbSet<Product> Products { get; set; }
+
         public DbSet<Country> Countries { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Avoid Warnings in Update Database
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            //To Disable Cascade Delete Rule 
+            var cascadeFKs = modelBuilder.Model
+                .G­etEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Casca­de);
+            foreach (var fk in cascadeFKs)
+            {
+                fk.DeleteBehavior = DeleteBehavior.Restr­ict;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
